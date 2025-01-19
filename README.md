b24e32a083b5e066d68cc5c0ded3231032ba9e2c
Tevinthuku <tevinthuku@gmail.com>
2025-01-19 07:32:07 +0300

    add readme

--- README.md
+++ README.md
@@ -19,11 +19,17 @@
 #### Run the load balancer
 
 ```bash
- go run cmd/loadbalancer/loadbalancer.go
+LB_PORT=8080 BACKEND_PORTS=8081,8082,8083 go run cmd/loadbalancer/loadbalancer.go
 ```
 
 #### Test request distribution by the load balancer to the different backends
 
-```
+```bash
 curl http://localhost:8080/test
+```
+
+```bash
+backend  http://localhost:8081  handling request
+backend  http://localhost:8082  handling request
+backend  http://localhost:8083  handling request
 ```
--- cmd/loadbalancer/loadbalancer.go
+++ cmd/loadbalancer/loadbalancer.go
@@ -1,12 +1,23 @@
 package main
 
 import (
+	"fmt"
 	"load_balancer/lb"
 	"log"
+	"os"
+	"strings"
 )
 
 func main() {
-	lb := lb.NewLoadBalancer(":8080", []string{"http://localhost:8081", "http://localhost:8082", "http://localhost:8083"})
+
+	lb_port := os.Getenv("LB_PORT")
+	lb_address := fmt.Sprintf(":%s", lb_port)
+	backend_ports := os.Getenv("BACKEND_PORTS")
+	backend_addresses := []string{}
+	for _, port := range strings.Split(backend_ports, ",") {
+		backend_addresses = append(backend_addresses, fmt.Sprintf("http://localhost:%s", port))
+	}
+	lb := lb.NewLoadBalancer(lb_address, backend_addresses)
 
 	if err := lb.Start(); err != nil {
 		log.Fatal(err)
